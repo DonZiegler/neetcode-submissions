ba2d9d4daec14a5c4fbd1c6411c74317c8ac13c5
DonZiegler <daziegler66@gmail.com>
2026-04-16 17:41:52 -0500

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length())
+            return false;
+        if (s.length()==0 || t.length()==0)
+            return true;
+        bool found=false;
+        for (int i=0; i<s.length(); i++) {
+            found=false;
+            for (int j=0; j<t.length(); j++) {
+                if (s[i]==t[j]) {
+                  t[j]=' ';
+                  found=true;
+                  break;
+                }              
+            }
+            if (found==false)
+                return false;
+        }
+        return true;
+    }
+};
